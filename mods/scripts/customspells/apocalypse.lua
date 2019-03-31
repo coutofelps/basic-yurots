0192ef5f90e0ed373ca0c3472fde2ce3c19705a5
Marcos Couto <0709.marcoscouto@gmail.com>
2019-03-31 03:45:12 -0300

    Editing and indenting the mods directory files

--- mods/scripts/customspells/apocalypse.lua
+++ mods/scripts/customspells/apocalypse.lua
@@ -1,20 +1,9 @@
 local combat = createCombatObject()
 
-arr = {
-{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-{0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
-{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1},
-{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-{0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
-{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-}
+arr = {{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},}
 
 local area = createCombatArea(arr)
+
 setCombatArea(combat, area)
 
 function spellCallback(param)
@@ -25,15 +14,18 @@ function spellCallback(param)
 
 	if(param.count < 5) then
 		param.count = param.count + 1
+
 		addEvent(spellCallback, math.random(1000, 4000), param)
 	end
 end
 
 function onTargetTile(cid, pos)
 	local param = {}
+
 	param.cid = cid
 	param.pos = pos
 	param.count = 0
+
 	spellCallback(param)
 end
 
